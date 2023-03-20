2a09b8712fa68e9522edebfab5359cc524d519f1
JoaoPedroFortes <jpedrofortes@outlook.com>
2023-03-19 21:41:51 -0300

    alteração na home, esboço de totalizadores e css geral

--- src/main/resources/static/js/extraFunctions.js
+++ src/main/resources/static/js/extraFunctions.js
@@ -1,12 +1,22 @@
 $('#modalExcluir').on('show.bs.modal', function (event) {
 	  var button = $(event.relatedTarget)
-	  var idObj = button.data('idobj') // Lê info dos atributos data-*
-	  var obs = button.data('obs')
-	  var action = button.data('modal-action');
-	  var modal = $(this)
-	  modal.find('form #idObj').val(idObj)
-	  modal.find('.modal-body span').text(obs)
-	  if(action != null){
-		  modal.find('form').attr('action',action);  
-	  }	  
-	});
+	var idObj = button.data('idobj') // Lê info dos atributos data-*
+	var obs = button.data('obs')
+	var action = button.data('modal-action');
+	var modal = $(this)
+	modal.find('form #idObj').val(idObj)
+	modal.find('.modal-body span').text(obs)
+	if (action != null) {
+		modal.find('form').attr('action', action);
+	}
+});
+
+const setDay = () => {
+	const day = document.getElementById('day');
+	if(!day) return;
+
+	const now = new Intl.DateTimeFormat('pt-BR', { dateStyle: 'full' }).format(new Date());
+	day.textContent = now;
+}
+
+setDay();
